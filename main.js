cf6025410a144c36664085b20e5667ccb116b552
shyamgarg <92663424+shyamgarg@users.noreply.github.com>
2022-05-01 07:53:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var canvas = new fabric.Canvas('myCanvas');
  
 function new_image()
 {
-	fabric.Image.fromURL('Birthday Image.jpg', function(Img) { 
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) { 
         block_image_object = Img;
         block_image_object.scaleToWidth (700); 
         block_image_object.scaleToHeight (510);
